0baa88327d1f910eead82622fae96b2dd7faddf7
SharonRoja33 <pekanasharonroja3@gmail.com>
2021-09-11 10:15:11 +0530

    add user api

--- server/index.js
+++ server/index.js
@@ -14,7 +14,10 @@ import googleAuthConfig from "./config/google.config";
 import Auth from "./API/Auth";
 import Restaurant from "./API/Restaurant";
 import Food from "./API/Food";
-import Image from "./API/Image"
+import Image from "./API/Image";
+import Order from "./API/orders";
+import Reviews from "./API/reviews";
+import User from "./API/User";
 
 // Database connection
 import  ConnectDB from "./database/connection";
@@ -38,6 +41,10 @@ zomato.use("/auth", Auth);
 zomato.use("/restaurant", Restaurant);
 zomato.use("/food", Food);
 zomato.use("/image", Image);
+zomato.use("/order", Order);
+zomato.use("/reviews", Reviews);
+zomato.use("/user", User);
+
 
 zomato.get("/", (req, res) => res.json({ message: "Setup Success" }));
 
