ef67579601d484fd3695a9a50bbc73a1d604c629
SharonRoja33 <pekanasharonroja3@gmail.com>
2021-09-11 09:03:19 +0530

    AWS s3

--- server/Utils/AWS/s3.js
+++ server/Utils/AWS/s3.js
@@ -2,7 +2,7 @@ import AWS from "aws-sdk";
 
 require('dotenv').config();
 
-// AWS s3 bucket config
+//  AWS s3 bucket config
 const s3Bucket = new AWS.S3({
     accessKeyId: process.env.AWS_S3_ACCESS_KEY,
     secretAccessKey: process.env.AWS_S3_SECRET_KEY,
