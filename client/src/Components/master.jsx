849eab762b666fb3af95288a9675c4b6b83031d4
SharonRoja33 <pekanasharonroja3@gmail.com>
2021-09-13 11:50:51 +0530

    add pc screen food tabs

---
+++ client/src/Components/master.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useParams  } from 'react-router-dom';
+export const Master = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Master;
