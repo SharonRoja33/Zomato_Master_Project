a0bcfa64fd7bdb5de4335f4394a72ee83b3b24c4
SharonRoja33 <pekanasharonroja3@gmail.com>
2021-09-13 19:30:42 +0530

    add restaurant grid

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,24 @@
+import { Route, Redirect } from "react-router-dom";
+
 // HOC
 import HomeLayoutHOC from "./HOC/Home.Hoc";
+import RestaurantLayoutHOC from "./HOC/Restaurant.HOC";
 
 // Component
 import Temp from "./Components/temp";
-import Master from "./Components/master";
 
+// pages
+import Home from "./Page/Home";
 
 function App() {
   return (
     <>
-      <HomeLayoutHOC path = "/" exact component={Temp} />
-      <HomeLayoutHOC path = "/:type" exact component={Master} />
-
-     </>
+      <Route path="/" exact>
+        <Redirect to="/delivery" />
+      </Route>
+      <HomeLayoutHOC path = "/:type" exact component={Home} />
+      <RestaurantLayoutHOC path = "/restaurant/:id" exact component={Temp} />
+    </>
   );
     
 }
